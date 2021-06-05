a551bd4df2cf431382ef251119e7f6819a6c22b6
simenraes <simieraes@hotmail.com>
2021-06-06 01:35:16 +0200

    geen appartement gekozen (gefixt)

--- src/main/java/be/thomasmore/appartementverhuur/repositories/AppartementRepository.java
+++ src/main/java/be/thomasmore/appartementverhuur/repositories/AppartementRepository.java
@@ -4,7 +4,9 @@ import be.thomasmore.appartementverhuur.model.Appartement;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AppartementRepository extends CrudRepository<Appartement, Integer> {
     List<Appartement> findAllBy();
+    Optional<Appartement> findById(Integer id);
 }
