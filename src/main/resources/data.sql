2c0e80cc2511592972dc3edbf167e12cf9537ae5
simenraes <simieraes@hotmail.com>
2021-06-07 02:48:58 +0200

    properties wijziging

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -20,4 +20,4 @@ insert into Appartement (
     ID, CAPACITY, APPARTEMENT_NAAM, STAD, IS_PARKING_BESCHIKBAAR, AFSTAND_TOT_CENTRUM, EXTRA_INFO, PRIJS_PER_MAAND,
     AANTAL_SLAAPKAMERS, OPPERVLAKTE, HUISDIEREN_TOEGELATEN)
 values (
-           4, 2, 'testnaam3', 'Antwerpen', true, 50, 'info', 500, 3, 30, true);
+           4, 2, 'testnaam3', 'Antwerpen', true, 58, 'info', 500, 3, 30, true);
