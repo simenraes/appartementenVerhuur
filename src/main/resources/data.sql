779981d833c25abdc6a9bf494b9c2c75f8d21b59
simenraes <simieraes@hotmail.com>
2021-06-07 13:46:45 +0200

    sql wijziging

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,22 +2,22 @@ insert into Appartement (
     ID, CAPACITY, APPARTEMENT_NAAM, STAD, IS_PARKING_BESCHIKBAAR, AFSTAND_TOT_CENTRUM, EXTRA_INFO, PRIJS_PER_MAAND,
     AANTAL_SLAAPKAMERS, OPPERVLAKTE, HUISDIEREN_TOEGELATEN)
 values (
-           0, 2, 'testnaam', 'Antwerpen', true, 50, 'info', 500, 3, 30, true);
+           1, 2, 'testnaam', 'Antwerpen', true, 50, 'info', 500, 3, 30, true);
 
 insert into Appartement (
     ID, CAPACITY, APPARTEMENT_NAAM, STAD, IS_PARKING_BESCHIKBAAR, AFSTAND_TOT_CENTRUM, EXTRA_INFO, PRIJS_PER_MAAND,
     AANTAL_SLAAPKAMERS, OPPERVLAKTE, HUISDIEREN_TOEGELATEN)
 values (
-           1, 2, 'testnaam1', 'Antwerpen', false, 50, 'info', 300, 3, 30, false);
+           2, 2, 'testnaam1', 'Antwerpen', false, 50, 'info', 300, 3, 30, false);
 
 insert into Appartement (
     ID, CAPACITY, APPARTEMENT_NAAM, STAD, IS_PARKING_BESCHIKBAAR, AFSTAND_TOT_CENTRUM, EXTRA_INFO, PRIJS_PER_MAAND,
     AANTAL_SLAAPKAMERS, OPPERVLAKTE, HUISDIEREN_TOEGELATEN)
 values (
-           2, 2, 'testnaam2', 'Antwerpen', true, 30, 'info', 500, 2, 30, false);
+           3, 2, 'testnaam2', 'Antwerpen', true, 30, 'info', 500, 2, 30, false);
 
 insert into Appartement (
     ID, CAPACITY, APPARTEMENT_NAAM, STAD, IS_PARKING_BESCHIKBAAR, AFSTAND_TOT_CENTRUM, EXTRA_INFO, PRIJS_PER_MAAND,
     AANTAL_SLAAPKAMERS, OPPERVLAKTE, HUISDIEREN_TOEGELATEN)
 values (
-           3, 2, 'testnaam3', 'Antwerpen', true, 59, 'info', 500, 4, 30, true);
+           4, 2, 'testnaam3', 'Antwerpen', true, 59, 'info', 500, 4, 30, true);
