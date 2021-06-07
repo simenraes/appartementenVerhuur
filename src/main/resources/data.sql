08ad8967e9535a56893ba4273ec3cf98a31c4988
simenraes <simieraes@hotmail.com>
2021-06-07 02:09:54 +0200

    sql aangepast

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,7 +14,7 @@ insert into Appartement (
     ID, CAPACITY, APPARTEMENT_NAAM, STAD, IS_PARKING_BESCHIKBAAR, AFSTAND_TOT_CENTRUM, EXTRA_INFO, PRIJS_PER_MAAND,
     AANTAL_SLAAPKAMERS, OPPERVLAKTE, HUISDIEREN_TOEGELATEN)
 values (
-           3, 2, 'testnaam2', 'Antwerpen', true, 40, 'info', 500, 3, 20, false);
+           3, 2, 'testnaam2', 'Antwerpen', true, 40, 'info', 500, 2, 20, false);
 
 insert into Appartement (
     ID, CAPACITY, APPARTEMENT_NAAM, STAD, IS_PARKING_BESCHIKBAAR, AFSTAND_TOT_CENTRUM, EXTRA_INFO, PRIJS_PER_MAAND,
