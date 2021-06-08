734210642bc8907bf5489ff0919f2bbd91a6f848
simenraes <simieraes@hotmail.com>
2021-06-08 22:26:10 +0200

    modelattribute + th object

--- src/main/java/be/thomasmore/appartementverhuur/controllers/AdminController.java
+++ src/main/java/be/thomasmore/appartementverhuur/controllers/AdminController.java
@@ -2,6 +2,8 @@ package be.thomasmore.appartementverhuur.controllers;
 
 import be.thomasmore.appartementverhuur.model.Appartement;
 import be.thomasmore.appartementverhuur.repositories.AppartementRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,18 +14,32 @@ import java.util.Optional;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+    private Logger logger = LoggerFactory.getLogger(AdminController.class);
+
     @Autowired
     private AppartementRepository appartementRepository;
+
+    @ModelAttribute("appartement")
+    public Appartement findAppartement(@PathVariable(required = false) Integer id) {
+        logger.info("findAppartement " + id);
+
+
+        Optional<Appartement> optionalAppartement = appartementRepository.findById(id);
+        if (optionalAppartement.isPresent())
+            return optionalAppartement.get();
+        return null;
+    }
 
     @GetMapping("/appartementedit/{id}")
     public String appartementEdit(Model model,
                                   @PathVariable int id) {
 
-        Optional<Appartement> optionalAppartement = appartementRepository.findById(id);
-        if (optionalAppartement.isPresent()) {
-            model.addAttribute("appartement", optionalAppartement.get());
+//        Optional<Appartement> optionalAppartement = appartementRepository.findById(id);
+//        if (optionalAppartement.isPresent()) {
+//            model.addAttribute("appartement", optionalAppartement.get());
+//
+//        }
 
-        }
         return "admin/appartementedit";
     }
     @PostMapping("/appartementedit/{id}")
