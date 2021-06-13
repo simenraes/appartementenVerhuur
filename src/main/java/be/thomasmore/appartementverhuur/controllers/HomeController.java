914a26da341320a9c05ee391c0028ea392adbfae
simenraes <simieraes@hotmail.com>
2021-06-13 22:25:03 +0200

    logout css

--- src/main/java/be/thomasmore/appartementverhuur/controllers/HomeController.java
+++ src/main/java/be/thomasmore/appartementverhuur/controllers/HomeController.java
@@ -1,22 +1,30 @@
 package be.thomasmore.appartementverhuur.controllers;
 
+import be.thomasmore.appartementverhuur.model.User;
+import be.thomasmore.appartementverhuur.repositories.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
     private Logger logger = LoggerFactory.getLogger(HomeController.class);
+    @Autowired
+    private UserRepository userRepository;
 
 
     @GetMapping({"/", "/home"})
     public String home(Model model, Principal principal) {
+
         final String loginName = (principal !=null)? principal.getName() : "NOBODY";
         logger.info("homelogged in" +loginName);
+
         model.addAttribute("loginName", loginName);
 
 
